23c0739e62e62f555c26138964a408ff45e2b88b
Mr.RySes <46703268+RyannKim327@users.noreply.github.com>
2022-10-21 13:38:51 +0000

    10-21-22 21:30 Modify with parser

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express")
 const fs = require("fs")
-const parse = require("body-parser")
+const parser = require("body-parser")
 const app = express()
+const enc = parser.urlencoded({extended: false})
 
 app.get("/", (req, res) => {
 	res.send("This is a landing page...")
 })
 
-app.post("/feed", parse, (req, res) => {
+app.post("/feed", enc, (req, res) => {
 	let json = JSON.parse(fs.readFileSync("data.json", "utf8"))
 	let data = {
 		feed: req.body.error,
